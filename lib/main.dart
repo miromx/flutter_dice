99e02a3015baed1f49ff079ac5dc0544ae7c2ae5
Laptop <Laptop@example.com>
2020-06-09 00:02:39 +0300

    third

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.red[900],
       appBar: AppBar(
         title: Text('Dice'),
         backgroundColor: Colors.red,
@@ -16,8 +17,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.asset('images/six.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/six.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/one.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
