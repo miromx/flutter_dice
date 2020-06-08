9c0a0c4cef66339357298c1c69476bd02d950e89
PC <pc@example.com>
2020-06-08 20:36:42 +0300

    First

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dice'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('images/six.png'),
+    );
+  }
+}
