268c0a182b1c086c219e194c1f7eeaafe2af7fb1
PC <pc@example.com>
2020-06-09 16:46:14 +0300

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.red[900],
       appBar: AppBar(
-        title: Text('Dice'),
-        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text('Кубики'),
+        backgroundColor: Colors.red[900],
       ),
       body: DicePage(),
     ),
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/six.png'),
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/one.png'),
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/$rightDiceNumber.png'),
             ),
           ),
         ],
